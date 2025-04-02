dd5419f800caa56e47ef04fcba63da4395b877b0
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-04-02 21:00:47 +0300

    update: cs2screenmenuapi

--- MVP-Anthem/src/MVP-Anthem.cs
+++ MVP-Anthem/src/MVP-Anthem.cs
@@ -7,7 +7,7 @@ public partial class MVPAnthem : BasePlugin
 {
     public override string ModuleAuthor => "T3Marius";
     public override string ModuleName => "MVP-Anthem";
-    public override string ModuleVersion => "1.0.1";
+    public override string ModuleVersion => "1.0.2";
     public PluginConfig Config { get; set; } = new PluginConfig();
     public static MVPAnthem Instance { get; set; } = new MVPAnthem();
     public override void Load(bool hotReload)
